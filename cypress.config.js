21015896eb3543b8f9785e1cbfb733c04d511a16
Viacheslav Voronin <v.vvoronin@firstsport.io>
2023-06-20 19:53:26 +0300

    new commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require('cypress')
+const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
 module.exports = defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
+    reporter: 'mochawesome',
+    reporterOptions: {
+        reportDir: 'cypress/results',
+        overwrite: true,
+        html: true,
+        json: true
+    },
     e2e: {
         setupNodeEvents(on, config) {
-            // implement node event listeners here
+            allureWriter(on, config)
+            return config
         }
     }
 })
